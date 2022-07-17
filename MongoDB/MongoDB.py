847947c12848460b51e273b9d61be441ab7181fb
SatabdoM <satabdomajumdertech@gmail.com>
2022-07-17 19:43:21 +0530

    this is my first code push in git

--- MongoDB/MongoDB.py
+++ MongoDB/MongoDB.py
@@ -9,6 +9,21 @@ d={
     "email":"satabdomajumdar@gmail.com",
     "surname":"majumder"
 }
+d={
+    "name":"satabdo",
+    "email":"satabdomajumdar@gmail.com",
+    "surname":"majumder"
+}
+d={
+    "name":"satabdo",
+    "email":"satabdomajumdar@gmail.com",
+    "surname":"majumder"
+}
+d={
+    "name":"satabdo",
+    "email":"satabdomajumdar@gmail.com",
+    "surname":"majumder"
+}
 db1=client['mongotest']
 coll=db['test']
 
